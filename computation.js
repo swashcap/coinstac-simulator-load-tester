103d7dc9fb70d9473dec71c09c503795d5ad6087
Cory Reed <creed@mrn.org>
2016-08-23 14:49:40 -0700

    Add time tracking to computation.

--- computation.js
+++ computation.js
@@ -6,6 +6,8 @@ const fs = require('fs');
 const path = require('path');
 const pkg = require('./package.json');
 
+const MAX_ITERATIONS = 20;
+
 module.exports = {
   local: {
     /**
@@ -14,25 +16,24 @@ module.exports = {
      * @param {Object} params
      * @param {Object} params.remoteResult
      * @param {string} [params.previousData]
-     * @returns {Promise} Resolves to a string
+     * @returns {Promise} Resolves to an array containing the hash and the
+     * duration of the computation.
      */
     fn: params => {
       return new Promise((resolve, reject) => {
         const hasher = crypto.createHash('sha256');
         const reader = fs.createReadStream(path.join(__dirname, 'randomBytes'));
+        const start = Date.now();
         reader.on('error', reject);
-        reader.pipe(hasher).pipe(concatStream(resolve));
+        reader.pipe(hasher).pipe(concatStream(hash => {
+          resolve([hash.toString('hex'), Date.now() - start]);
+        }));
       })
-        .then(
-          hash => {
-            console.log(hash);
-            return hash;
-          }, 
-          error => {
-            console.error(error);
-            throw error;
-          }
-        );
+        // TODO: Determine whether coinstac-common catches rejections
+        .catch(error => {
+          console.error(error);
+          throw error;
+        });
     },
     type: 'function',
   },
@@ -45,6 +46,8 @@ module.exports = {
      * @param {Object} params
      * @param {string[]} params.usernames
      * @param {Object[]} params.userResults
+     * @param {Object} params.userResults.data Results from the `local`
+     * computation.
      * @param {Object} [params.previousData]
      * @returns {Object}
      */
@@ -54,15 +57,37 @@ module.exports = {
         {
           complete: false,
           iteration: 0,
+          localTimes: [],
+          start: Date.now(),
         },
         params.previousData
       );
 
+      // Add current iteration's times
+      if (Array.isArray(params.userResults)) {
+        const localTimes = params.userResults.reduce((memo, userResult) => {
+          return ('data' in userResult && Array.isArray(userResult.data)) ?
+            memo.concat(userResult.data[1]) :
+            memo;
+        }, []);
+
+        Array.prototype.push.apply(result.localTimes, localTimes);
+      }
+
       result.iteration++;
 
-      // Mark pipeline step as 'complete' after 100 iterations.
-      if (result.iteration >= 2) {
+      // Mark pipeline step as 'complete' max iterations:
+      if (result.iteration >= MAX_ITERATIONS) {
         result.complete = true;
+
+        const averageLocalTime = Math.round(
+          result.localTimes.reduce((total, localTime) => total += localTime) /
+          result.localTimes.length
+        );
+        const time = Date.now() - result.start;
+
+        console.log(`${MAX_ITERATIONS} iterations complete in ${time}ms`);
+        console.log(`Average client time: ${averageLocalTime}ms`);
       }
 
       return result;
